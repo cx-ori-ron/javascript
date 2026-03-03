af818f4df2d7cc6d532c2c1ca3b75c78ee2bf6b4
Ori Ron <25453080+cx-ori-ron@users.noreply.github.com>
2026-03-03 11:59:16 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert(window.location.href + "\n\n" + document.cookie);
+function getCookieValByName(name) {
+  const match = document.cookie.match(new RegExp('(?:^|;\\s*)' + name + '=([^;]*)'));
+  return match ? decodeURIComponent(match[1]) : null;
+}
+
+alert(getCookieValByName("logged_in"));
+alert(getCookieValByName("JSESSIONID"));
+alert(getCookieValByName("user_session"));
